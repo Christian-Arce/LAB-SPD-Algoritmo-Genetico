6fdc73de201f220f74fd7bf21537d6078c68b476
Christian-Arce <80466908+Christian-Arce@users.noreply.github.com>
2024-05-30 23:54:41 -0400

    Update grafico.py

--- grafico.py
+++ grafico.py
@@ -2,9 +2,9 @@ import matplotlib.pyplot as plt
 
 # Datos de ejemplo (reemplaza con los resultados reales)
 sizes = ['Pequeño', 'Mediano', 'Grande']
-sequential_times = [0.1, 1.0, 10.0]  # Reemplaza con los tiempos reales
-parallel_times = [0.05, 0.5, 5.0]    # Reemplaza con los tiempos reales
-num_threads = 4
+sequential_times = [0.008, 19.123, 72.728]  # tiempos secuenciales
+parallel_times = [0.030, 7.520, 29.504]    # tiempos paralelos
+num_threads = 16
 
 # Cálculo de Speedup y Eficiencia
 speedup = [seq / par for seq, par in zip(sequential_times, parallel_times)]
